6653ca453b25d19d10ed9c091b39528e11c3ded2
ohs80340 <ohs80340@ohs80340noMacBook-Air.local>
2022-07-24 15:02:33 +0900

    add raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::{stdin, stdout, Write};
+use termion::event::{Event, Key};
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = stdin();
+
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+    for event in stdin.events() {
+        if event.unwrap() == Event::Key(Key::Ctrl('c')) {
+            return;
+        }
+    }
 }
